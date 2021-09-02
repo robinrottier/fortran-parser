bb04c1d5a4936c7235bde2248bfd313cfbb05e70
Robin Rottier <robin@rottier.co.uk>
2021-09-02 10:13:46 +0100

    Add csproj targets to inc g4 antlr file sint he build

--- antlr4-fortran/antlr4.bat
+++ antlr4-fortran/antlr4.bat
@@ -1,5 +1,9 @@
+@echo off
 @setlocal
+
 set d0=%~dp0
+
+echo ANTLR4: %*
 
 set CLASSPATH=%d0%\antlr-4.9.2-complete.jar;
 java org.antlr.v4.Tool %*
--- antlr4-fortran/buildcsharp.bat
+++ antlr4-fortran/buildcsharp.bat
@@ -1,9 +1,19 @@
+@echo off
 @setlocal
 
 set f=-Dlanguage=CSharp -o csharp 
 if not exist csharp md csharp
 
-echo Building lexer...
-call antlr4 %f% Fortran77Lexer.g4
-echo Building parser...
-call antlr4 %f% Fortran77Parser.g4
+if "%1" == "" (
+	echo Nohting to build!!
+) else (
+	if /i "%1" == "/a" (
+		echo Building lexer...
+		call antlr4 %f% Fortran77Lexer.g4
+		echo Building parser...
+		call antlr4 %f% Fortran77Parser.g4
+	) else (
+		echo Building %1...
+		call antlr4 %f% %1
+	)
+)
